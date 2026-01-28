21589d720b3aab72eafd42cec5a03d236c43b572
Vaibhav Rote <vsr3549@gmail.com>
2026-01-28 09:43:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+
 function insert_Row() {
     //Write your code here
+
+	//let cell1= document.createElement("tr")
+
+	let cell=document.createElement("tr");
+	let cell1=document.createElement("td");
+	let cell2=document.createElement("td");
+	cell1.textContent="New Cell1";
+	cell2.textContent="New Cell2";
+	let table=document.querySelector("#sampleTable")
+	cell.append(cell1,cell2)
+	table.prepend(cell)
+	
   
   
 }
+
+//let input=document.querySelector("input")
+
